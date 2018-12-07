8e41b349146e59ff452d81af4725efa36f5396b3
Daniel <dnlmrls9@gmail.com>
2018-12-07 15:09:40 +0100

    bug fix: profile component was not working

--- app/src/app/shared/components/users/user-modal.component.ts
+++ app/src/app/shared/components/users/user-modal.component.ts
@@ -1,8 +1,9 @@
 import {Component, OnInit, Output, EventEmitter, Input} from "@angular/core";
 import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {UserService, ExchangeUserService, UserHelperService} from "../../services/";
+import {UserService, ExchangeUserService} from "../../services/";
 import {User} from "../../model";
 import {NotificationService} from "../../../services";
+import {UserHelperService} from "../../services/user-helper.service";
 
 @Component({
     selector: 'user-modal',
