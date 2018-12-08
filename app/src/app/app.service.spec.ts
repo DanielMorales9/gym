8e83f700956271eeb080c6c0ba49719ab6e31eae
Daniel <dnlmrls9@gmail.com>
2018-12-08 13:42:05 +0100

    authentication services refactored and tested

--- app/src/app/app.service.spec.ts
+++ app/src/app/app.service.spec.ts
@@ -27,115 +27,6 @@ describe('AppService', () => {
         backend = TestBed.get(HttpTestingController);
     });
 
-    describe ("testing #changePassword method", () => {
-        it("it should change password", done => {
-            let user = new User();
-            user.email = email;
-            user.password = 'giovannigiorgio';
-            let userType = 'customer';
-            appService.changePassword(user, userType).subscribe((res) => {
-                expect(user.password).toBe(undefined);
-                expect(user.email).toEqual(email);
-                done()
-            });
-
-            let req = backend.expectOne({
-                url: "/authentication/customer/changePassword",
-                method: "POST"
-            });
-
-            user.password = undefined;
-            req.flush(user)
-        });
-    });
-
-    describe ("testing #getAuthorizationHeader method", () => {
-        it("it should return Basic header", done => {
-            appService.credentials = {username: 'email', password: 'password'};
-            let header = appService.getAuthorizationHeader();
-            expect(header).not.toEqual('Basic ');
-            done()
-        });
-        it("it should return Empty Header", done => {
-            let header = appService.getAuthorizationHeader();
-            expect(header).toEqual('Basic ');
-            done()
-        });
-    });
-
-
-
-    describe ("testing #resendToken method", () => {
-        it("it should return empty user", done => {
-            let token = "aaaa";
-            appService.resendToken(token).subscribe((res) => {
-                expect(res).toEqual(new User());
-                done()
-            });
-
-            let req = backend.expectOne({
-                url: `/authentication/resendToken?token=${token}`,
-                method: "GET"
-            });
-
-            req.flush(new User())
-        });
-    });
-
-    describe ("testing #logout method", () => {
-        it("it should return nothing", done => {
-            appService.logout(() => {
-                expect(appService.authenticated).toBe(false);
-                done()
-            });
-
-            let req = backend.expectOne({
-                url: `/logout`,
-                method: "GET"
-            });
-
-            req.flush(null, {status: 200, statusText: "OK"})
-        });
-    });
-
-    describe ("testing #verifyPassword method", () => {
-        it("it should return empty user", done => {
-            let credentials = {username: email, password: "giovanni"};
-            appService.verifyPassword(credentials, 'customer').subscribe((res) => {
-                expect(res).toEqual(new User());
-                done()
-            });
-
-            let req = backend.expectOne({
-                url: `/authentication/customer/verifyPassword`,
-                method: "POST"
-            });
-
-            req.flush(new User())
-        });
-    });
-
-    describe ("testing #findByEmail method", () => {
-        it("it should return User", done => {
-            appService.findByEmail(email).subscribe((res) => {
-                let user = new User();
-                user.email = email;
-                user.firstName = 'giorgio';
-                expect(res).toEqual(user);
-                done()
-            });
-
-            let req = backend.expectOne({
-                url: `/authentication/findByEmail?email=${email}`,
-                method: "GET"
-            });
-            let user = new User();
-            user.email = email;
-            user.firstName = 'giorgio';
-            req.flush(user);
-        });
-    });
-
     describe ("testing #discardSession method", () => {
         it("it should discard session", done => {
             appService.discardSession();
@@ -197,5 +88,36 @@ describe('AppService', () => {
             req.flush(null, { status: 401, statusText: 'Unauthorized'})
         })
     });
+
+    describe ("testing #logout method", () => {
+        it("it should return nothing", done => {
+            appService.logout(() => {
+                expect(appService.authenticated).toBe(false);
+                done()
+            });
+
+            let req = backend.expectOne({
+                url: `/logout`,
+                method: "GET"
+            });
+
+            req.flush(null, {status: 200, statusText: "OK"})
+        });
+    });
+
+    describe ("testing #getAuthorizationHeader method", () => {
+        it("it should return Basic header", done => {
+            appService.credentials = {username: 'email', password: 'password'};
+            let header = appService.getAuthorizationHeader();
+            expect(header).not.toEqual('Basic ');
+            done()
+        });
+        it("it should return Empty Header", done => {
+            let header = appService.getAuthorizationHeader();
+            expect(header).toEqual('Basic ');
+            done()
+        });
+    });
+
 
 });
--- app/src/app/auth/services/auth.service.ts
+++ app/src/app/auth/services/auth.service.ts
@@ -1,35 +1,42 @@
 import {Injectable} from "@angular/core";
 import {HttpClient} from "@angular/common/http";
 import {User} from "../../shared/model";
+import {AppService} from "../../app.service";
 
 @Injectable()
 export class AuthService {
 
 
-    constructor(private http: HttpClient) {
+    constructor(private http: HttpClient, private appService: AppService) {
     }
 
     getUserFromVerificationToken(token: any) {
+        this.appService.credentials = undefined;
         return this.http.get("/authentication/verification",{ params: {token: token}});
     }
 
     verifyPassword(credentials, role) {
+        this.appService.credentials = undefined;
         return this.http.post( `/authentication/${role}/verifyPassword`, credentials);
     }
 
     resendToken(token: string) {
+        this.appService.credentials = undefined;
         return this.http.get("/authentication/resendToken", {params: {token: token}})
     }
 
     changePassword(user: User, userType: string) {
+        this.appService.credentials = undefined;
         return this.http.post(`/authentication/${userType}/changePassword`, user);
     }
 
     findByEmail(email: string) {
+        this.appService.credentials = undefined;
         return this.http.get(`/authentication/findByEmail?email=${email}`)
     }
 
     resendChangePasswordToken(token: string) {
+        this.appService.credentials = undefined;
         return this.http.get("/authentication/resendChangePasswordToken", {params: {token: token}})
     }
 
