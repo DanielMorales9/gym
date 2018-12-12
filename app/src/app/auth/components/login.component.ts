f1863737f43960f7dc59d626cd846bd74f192d1f
Daniel <dnlmrls9@gmail.com>
2018-12-12 15:24:15 +0100

    login form is now reactive

--- app/src/app/auth/components/login.component.ts
+++ app/src/app/auth/components/login.component.ts
@@ -1,22 +1,31 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import { Router } from '@angular/router';
 import {AppService} from "../../services";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 
 @Component({
     templateUrl: './login.component.html',
     styleUrls: ['../../app.component.css']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
     error = false;
+    form: FormGroup;
 
     credentials = {username: '', password: ''};
 
     constructor(private app: AppService,
+                private builder: FormBuilder,
                 private router: Router) {
     }
 
+    ngOnInit(): void {
+        this.buildForm()
+    }
+
     login() {
+        this.credentials.username = this.email.value;
+        this.credentials.password = this.password.value;
         this.app.authenticate(this.credentials, (isAuthenticated) => {
             if (!isAuthenticated) {
                 this.error = true;
@@ -30,4 +39,19 @@ export class LoginComponent {
         });
     }
 
+
+    private buildForm() {
+        this.form = this.builder.group({
+            email: [this.credentials.username, [Validators.email, Validators.required]],
+            password: [this.credentials.password, Validators.required]
+        })
+    }
+
+    get email() {
+        return this.form.get("email")
+    }
+
+    get password() {
+        return this.form.get("password")
+    }
 }
