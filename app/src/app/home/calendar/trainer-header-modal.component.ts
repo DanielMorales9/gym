b996dacd86b062ef963c917148f22c9b402ac60e
Daniel <dnlmrls9@gmail.com>
2018-12-20 20:20:51 +0100

    trainer calendar added

---
+++ app/src/app/home/calendar/trainer-header-modal.component.ts
@@ -0,0 +1,49 @@
+import {Component, OnInit} from "@angular/core";
+import {GymConfigurationService, NotificationService} from "../../services";
+import {BaseCalendarModal} from "./base-calendar-modal";
+import {TimesOffService} from "../../shared/services";
+
+@Component({
+    selector: 'trainer-header-modal',
+    templateUrl: './trainer-header-modal.component.html',
+    styleUrls: ['../../app.component.css']
+})
+export class TrainerHeaderModalComponent extends BaseCalendarModal implements OnInit {
+
+    private MODAL_BUTTON: string = 'trainer-header-modal-button';
+
+    timeOffName: string;
+
+    constructor(public notificationService: NotificationService,
+                private timesOffService: TimesOffService,
+                private gymConf: GymConfigurationService) {
+        super(notificationService);
+    }
+
+    ngOnInit(): void {
+        this.modalButton = this.MODAL_BUTTON
+    }
+
+    submit() {
+        this.loading = true;
+        let {startTime, endTime} = this.gymConf.getStartAndEndTimeByGymConfiguration(new Date(this.modalData.event.day.date));
+        let stringDate = (startTime);
+        this.timesOffService.book(startTime, endTime, 'trainer', this.timeOffName, this.modalData.userId)
+            .subscribe((res) => {
+                this.message = {
+                    text: `Ferie confermate per il ${stringDate}`,
+                    class: "alert-success"
+                };
+                this.event.emit();
+            }, (err) => {
+                this.message = {
+                    text: err.error.message,
+                    class: "alert-danger"
+                };
+                this.onComplete()
+            }, () => {
+                this.onComplete();
+            });
+    }
+
+}
---
+++ app/src/app/home/calendar/trainer-info-modal.component.ts
@@ -0,0 +1,73 @@
+import {Component, OnInit} from "@angular/core";
+import {BaseCalendarModal} from "./base-calendar-modal";
+import {DateService, NotificationService} from "../../services";
+import {TrainingService} from "../../shared/services";
+
+@Component({
+    selector: 'trainer-info-modal',
+    templateUrl: './trainer-info-modal.component.html',
+    styleUrls: ['../../app.component.css']
+})
+export class TrainerInfoModalComponent extends BaseCalendarModal implements OnInit {
+
+    private MODAL_BUTTON = 'trainer-info-modal-button';
+
+    constructor(private dateService: DateService,
+                private trainingService: TrainingService,
+                public notificationService: NotificationService) {
+        super(notificationService);
+    }
+
+    ngOnInit(): void {
+        this.modalButton = this.MODAL_BUTTON;
+    }
+
+    submit() {
+        if (!this.modalData.event.meta.session.completed && this.modalData.event.meta.confirmed)
+            this.complete();
+        else this.confirm();
+    }
+
+    confirm() {
+        this.loading = true;
+        let stringDate = this.dateService.getDate(this.modalData.event.start);
+        this.trainingService.confirm(this.modalData.event.meta.id)
+            .subscribe((res) => {
+                this.message = {
+                    text: `Prenotazione #${this.modalData.event.meta.id} confermata per il ${stringDate}`,
+                    class: "alert-success"
+                };
+                this.event.emit();
+            }, (err) => {
+                this.message = {
+                    text: err.error.message,
+                    class: "alert-danger"
+                };
+                this.onComplete();
+            }, () => {
+                this.onComplete();
+            })
+    }
+
+    complete() {
+        this.loading = true;
+        let stringDate = this.dateService.getDate(this.modalData.event.start);
+        this.trainingService.complete(this.modalData.event.meta.id)
+            .subscribe( (res) => {
+                this.message = {
+                    text: `Allenamento #" + ${this.modalData.event.meta.session.id} completato`,
+                    class: "alert-success"
+                };
+                this.event.emit();
+            }, (err) => {
+                this.message = {
+                    text: err.error.message,
+                    class: "alert-danger"
+                };
+                this.onComplete();
+            }, () => {
+                this.onComplete();
+            })
+    }
+
+}
