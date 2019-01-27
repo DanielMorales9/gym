05dff325eb37cf39ce29d174b43b5c54cce52ced
Daniel <dnlmrls9@gmail.com>
2019-01-27 20:34:06 +0100

    some more changes

--- app/src/main/java/it/goodfellas/ConnectorConfig.java
+++ app/src/main/java/it/goodfellas/ConnectorConfig.java
@@ -35,9 +35,9 @@ public class ConnectorConfig {
     private Connector redirectConnector() {
         Connector connector = new Connector(TomcatServletWebServerFactory.DEFAULT_PROTOCOL);
         connector.setScheme("http");
-        connector.setPort(8080);
+        connector.setPort(80);
         connector.setSecure(false);
-        connector.setRedirectPort(8443);
+        connector.setRedirectPort(443);
         return connector;
     }
 
