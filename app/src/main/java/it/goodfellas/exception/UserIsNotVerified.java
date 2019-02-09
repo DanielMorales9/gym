33822fd78cfb6be7b5e5f397c8ae33e2c77d14ac
Daniel <d.morales@ki.group.de>
2019-02-09 15:40:19 +0100

    bug fix: #8
    Change Password is disabled when user not verified

---
+++ app/src/main/java/it/goodfellas/exception/UserIsNotVerified.java
@@ -0,0 +1,11 @@
+package it.goodfellas.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class UserIsNotVerified extends RuntimeException {
+    public UserIsNotVerified(Long id) {
+        super("L'utente con id " + id + " non è verificato!");
+    }
+}
