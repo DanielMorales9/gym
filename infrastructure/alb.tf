8f594e4b42fff2b7732b724219c50380c4b185e1
Daniel <dnlmrls9@gmail.com>
2019-03-06 00:31:47 +0100

    #29 added redirect to https (#30)

--- infrastructure/alb.tf
+++ infrastructure/alb.tf
@@ -1,3 +1,44 @@
+resource "aws_acm_certificate" "acm_certificate" {
+  domain_name       = "${var.domain}"
+  validation_method = "DNS"
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+data "aws_route53_zone" "route53" {
+  name         = "${var.domain}."
+  private_zone = false
+}
+
+resource "aws_route53_record" "route53_record" {
+  depends_on = ["aws_acm_certificate.acm_certificate"]
+  name = "${lookup(aws_acm_certificate.acm_certificate.domain_validation_options[0], "resource_record_name")}"
+  type = "${lookup(aws_acm_certificate.acm_certificate.domain_validation_options[0], "resource_record_type")}"
+  records = ["${lookup(aws_acm_certificate.acm_certificate.domain_validation_options[0], "resource_record_value")}"]
+  zone_id = "${data.aws_route53_zone.route53.zone_id}"
+  ttl = 300
+}
+
+resource "aws_acm_certificate_validation" "acm_certificate_validation" {
+  certificate_arn = "${aws_acm_certificate.acm_certificate.arn}"
+  validation_record_fqdns = ["${aws_route53_record.route53_record.*.fqdn}" ]
+}
+
+resource "aws_route53_record" "route53_record_to_alb" {
+  zone_id = "${data.aws_route53_zone.route53.zone_id}"
+  name    = "${var.domain}"
+  type    = "A"
+
+  alias {
+    name                   = "${aws_alb.alb.dns_name}"
+    zone_id                = "${aws_alb.alb.zone_id}"
+    evaluate_target_health = true
+  }
+}
+
+
 resource "aws_alb_target_group" "alb_target_group" {
   name        = "${var.app_name}-alb-target-group"
   vpc_id      = "${aws_vpc.default.id}"
@@ -32,42 +73,35 @@ resource "aws_alb" "alb" {
   }
 }
 
-resource "aws_alb_listener" "alb_listener" {
+resource "aws_alb_listener" "alb_listener_https" {
   load_balancer_arn = "${aws_alb.alb.arn}"
-  port              = "80"
-  protocol          = "HTTP"
-  depends_on        = ["aws_alb_target_group.alb_target_group"]
+  certificate_arn   = "${aws_acm_certificate.acm_certificate.arn}"
+  protocol          = "HTTPS"
+  port              = "443"
 
   default_action {
     target_group_arn = "${aws_alb_target_group.alb_target_group.arn}"
     type             = "forward"
   }
+
+  depends_on        = ["aws_alb_target_group.alb_target_group"]
 }
 
-//resource "aws_security_group" "alb_security_group" {
-//  name        = "${var.app_name}_alb_sg"
-//  description = "controls access to the application ELB"
-//  vpc_id      = "${aws_vpc.default.id}"
-//
-//  ingress {
-//    from_port   = 80
-//    to_port     = 80
-//    protocol    = "tcp"
-//    cidr_blocks = ["0.0.0.0/0"]
-//  }
-//
-//  ingress {
-//    from_port   = 8
-//    to_port     = 0
-//    protocol    = "icmp"
-//    cidr_blocks = ["0.0.0.0/0"]
-//  }
-//
-//  egress {
-//    from_port   = 0
-//    to_port     = 0
-//    protocol    = "-1"
-//    cidr_blocks = ["0.0.0.0/0"]
-//  }
-//}
+resource "aws_alb_listener" "alb_listener_http" {
+  load_balancer_arn = "${aws_alb.alb.arn}"
+  protocol          = "HTTP"
+  port              = "80"
+
+  default_action {
+    type             = "redirect"
+
+    redirect {
+      status_code = "HTTP_301"
+      protocol = "HTTPS"
+      port = "443"
+    }
+  }
+
+  depends_on        = ["aws_alb_target_group.alb_target_group"]
+}
 
