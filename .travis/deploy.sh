b5a1d4ee2b82e6e905e58d2b4765b24f7d5a90e1
Daniel <dnlmrls9@gmail.com>
2019-03-10 13:42:15 +0100

    Travis  (#36)
    * travis ci/cd #24
    
    * autoremove
    
    * fixing warning

--- .travis/deploy.sh
+++ .travis/deploy.sh
@@ -1,11 +1,8 @@
 #!/usr/bin/env bash
 
-if [ "$TRAVIS_BRANCH" -e "master" ]; then
-    echo "========================="
-    echo "|    Apply Terraform    |"
-    echo "========================="
-    sh terraform init .infrastructure/
-    sh terraform plan .infrastructure/
-    sh terraform apply .infrastructure/
-
-fi
+echo "========================="
+echo "|    Apply Terraform    |"
+echo "========================="
+sh terraform init .infrastructure/
+sh terraform plan .infrastructure/
+sh terraform apply .infrastructure/
