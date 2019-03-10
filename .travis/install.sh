9db996b79be562041fba9b423fc5649b9ef4bd6b
Daniel <d.morales@ki.group.de>
2019-03-10 13:07:46 +0100

    fixing warning

--- .travis/install.sh
+++ .travis/install.sh
@@ -10,12 +10,11 @@ if [ "$TRAVIS_BRANCH" = "master" ]; then
     echo "|  Installing AWS-CLI   |"
     echo "========================="
     sudo snap install aws-cli --classic
-fi
-
-if [ "$TRAVIS_PULL_REQUEST" = "true" ] || [ "$TRAVIS_BRANCH" = "master" ]; then
     echo "========================="
     echo "|  Installing Terraform |"
     echo "========================="
     wget https://releases.hashicorp.com/terraform/0.11.12/terraform_0.11.12_linux_amd64.zip
     unzip terraform_0.11.12_linux_amd64.zip
 fi
+
+
