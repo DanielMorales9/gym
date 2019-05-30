32d3bf4a20ab8f061f817c97171d27745ca326b6
Daniel <dnlmrls9@gmail.com>
2019-05-30 23:54:43 +0200

    removed Rest Repository from Roles

--- app/src/main/java/it/gym/repository/RoleRepository.java
+++ app/src/main/java/it/gym/repository/RoleRepository.java
@@ -2,9 +2,9 @@ package it.gym.repository;
 
 import it.gym.model.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
-@RepositoryRestResource
+@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
 }
