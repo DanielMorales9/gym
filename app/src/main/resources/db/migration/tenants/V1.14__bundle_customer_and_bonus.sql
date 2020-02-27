01c76c8735c5553dec7015ae9c0fe266d575889e
Daniel <dnlmrls9@gmail.com>
2020-02-27 11:22:58 +0100

    improved hateaos test

--- app/src/main/resources/db/migration/tenants/V1.14__bundle_customer_and_bonus.sql
+++ app/src/main/resources/db/migration/tenants/V1.14__bundle_customer_and_bonus.sql
@@ -3,6 +3,12 @@ ALTER TABLE bundles ADD COLUMN n_deletions int;
 ALTER TABLE bundle_specs ADD COLUMN unlimited_deletions bool DEFAULT true;
 ALTER TABLE bundle_specs ADD COLUMN n_deletions int;
 
+UPDATE bundles SET unlimited_deletions = true WHERE bundles.unlimited_deletions IS NULL;
+UPDATE bundle_specs SET unlimited_deletions = true WHERE bundle_specs.unlimited_deletions IS NULL;
+
+ALTER TABLE bundles ALTER COLUMN unlimited_deletions SET NOT NULL;
+ALTER TABLE bundle_specs ALTER COLUMN unlimited_deletions SET NOT NULL;
+
 ALTER TABLE bundles ADD COLUMN user_id bigint;
 
 ALTER TABLE ONLY bundles
