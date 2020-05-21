004858f577365fc82810a93a599969be23e7c57b
Daniel <dnlmrls9@gmail.com>
2020-05-21 15:55:44 +0200

    Caching workouts and images (#146)

--- app/src/main/java/it/gym/service/WorkoutService.java
+++ app/src/main/java/it/gym/service/WorkoutService.java
@@ -4,12 +4,18 @@ import it.gym.exception.NotFoundException;
 import it.gym.model.Workout;
 import it.gym.repository.WorkoutRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.cache.annotation.Caching;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import javax.transaction.Transactional;
 import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 @Transactional
@@ -19,15 +25,32 @@ public class WorkoutService implements ICrudService<Workout, Long> {
     private WorkoutRepository repo;
 
     @Override
-    public Workout save(Workout var1) {
-        return repo.save(var1);
-    }
-
-    @Override
+    @CachePut(value = "workouts-single", key = "#result.id", condition="#result != null")
     public Workout findById(Long var1) {
         return repo.findById(var1).orElseThrow(() -> new NotFoundException("Workout", var1));
     }
 
+    @Caching (
+            put = {
+                    @CachePut(value = "workouts-single", key = "#result.id", condition="#result != null"),
+            },
+            evict = {
+                    @CacheEvict(value = "workouts-all", allEntries = true),
+                    @CacheEvict(value = "workouts-search", allEntries = true)
+            }
+    )
+    @Override
+    public Workout save(Workout var1) {
+        return repo.save(var1);
+    }
+
+    @Caching(
+            evict = {
+                    @CacheEvict(value = "workouts-single", key = "#var1.id"),
+                    @CacheEvict(value = "workouts-all", allEntries = true),
+                    @CacheEvict(value = "workouts-search", allEntries = true)
+            }
+    )
     @Override
     public void delete(Workout var1) {
         repo.delete(var1);
@@ -38,8 +61,62 @@ public class WorkoutService implements ICrudService<Workout, Long> {
         return repo.findAll();
     }
 
+    @Cacheable(value = "workouts-all")
     public Page<Workout> findAll(Pageable pageable) {
         return repo.findAll(pageable);
+    }
+
+    public Page<Workout> search(String name, String tag, Boolean isTemplate, Pageable pageable) {
+        Page<Workout> page;
+
+        // all query params
+        if (tag != null && name != null) {
+            page = filterAndPageWorkoutsByQueryParams(name, tag, isTemplate, pageable);
+        }
+
+        // name or template
+        if (name != null && isTemplate != null) {
+            page = this.searchByNameAndTemplate(name, isTemplate, pageable);
+        }
+        if (name != null) {
+            page = this.searchByName(name, pageable);
+        }
+
+        // tag or template
+        if (tag != null && isTemplate != null) {
+            page = this.filterByNameAndTemplate(tag, isTemplate, pageable);
+        }
+        if (tag != null) {
+            page = this.filterByName(tag, pageable);
+        }
+
+        // only template or all
+        if (isTemplate != null) {
+            page = this.findByIsTemplate(isTemplate, pageable);
+        }
+        else {
+            page = this.findAll(pageable);
+        }
+
+        return initAssociation(page);
+
+    }
+
+    private Page<Workout> filterAndPageWorkoutsByQueryParams(String name, String filter, Boolean isTemplate, Pageable pageable) {
+        List<Workout> workouts = findAll()
+                .stream()
+                .filter(workout -> (isTemplate == null) || workout.isTemplate() == isTemplate)
+                .filter(workout -> workout.getName().contains(name))
+                .filter(workout ->
+                        (workout.getTag1() != null && workout.getTag1().equals(filter)) ||
+                                (workout.getTag2() != null && workout.getTag2().equals(filter)) ||
+                                (workout.getTag3() != null && workout.getTag3().equals(filter)))
+                .collect(Collectors.toList());
+        return new PageImpl<>(workouts, pageable, workouts.size());
+    }
+
+    private Page<Workout> initAssociation(Page<Workout> page) {
+        return page.map(Workout::eager);
     }
 
     public Page<Workout> searchByNameAndTemplate(String query, Boolean isTemplate, Pageable pageable) {
@@ -61,4 +138,5 @@ public class WorkoutService implements ICrudService<Workout, Long> {
     public Page<Workout> findByIsTemplate(Boolean isTemplate, Pageable pageable) {
         return repo.findByIsTemplateIs(isTemplate, pageable);
     }
+
 }
