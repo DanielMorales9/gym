7edf52b881e80fe77b3fc54a7b5c54c40adacf7e
Daniel <dnlmrls9@gmail.com>
2020-05-27 20:02:31 +0200

    refactoring completed

--- app/src/main/resources/db/migration/tenants/V1.19__purchase_option.sql
+++ app/src/main/resources/db/migration/tenants/V1.19__purchase_option.sql
@@ -1,6 +1,8 @@
 ALTER TABLE options ADD COLUMN option_type varchar;
+UPDATE options
+SET option_type = 'T';
 
-INSERT INTO options (option_id, bundle_spec_id, name, price, number, created_at, option_type)
+INSERT INTO options(option_id, bundle_spec_id, name, price, number, created_at, option_type)
 SELECT nextval('options_id_seq'), b.bundle_spec_id, b.name, b.price, b.num_sessions, now(), 'B'
 FROM bundle_specs as b
 WHERE b.bundle_spec_type = 'P';
