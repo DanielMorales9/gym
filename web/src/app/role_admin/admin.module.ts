d838f7a20c08cd523e2c8075ff65088dd6cb6eb0
Daniel <dnlmrls9@gmail.com>
2020-07-13 23:06:04 +0200

    angular10 (#198)

--- web/src/app/role_admin/admin.module.ts
+++ web/src/app/role_admin/admin.module.ts
@@ -1,20 +1,20 @@
-import {NgModule} from '@angular/core';
-import {CommonModule, registerLocaleData} from '@angular/common';
+import {Injectable, LOCALE_ID, NgModule} from '@angular/core';
+import {CommonModule} from '@angular/common';
 import {AdminRouting} from './admin.routing';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import { MatButtonModule } from '@angular/material/button';
-import { MatCardModule } from '@angular/material/card';
-import { MAT_CHECKBOX_CLICK_ACTION, MatCheckboxModule } from '@angular/material/checkbox';
-import { MatDialogModule } from '@angular/material/dialog';
-import { MatDividerModule } from '@angular/material/divider';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatIconModule } from '@angular/material/icon';
-import { MatInputModule } from '@angular/material/input';
-import { MatListModule } from '@angular/material/list';
-import { MatSelectModule } from '@angular/material/select';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
-import { MatToolbarModule } from '@angular/material/toolbar';
+import {MatButtonModule} from '@angular/material/button';
+import {MatCardModule} from '@angular/material/card';
+import {MAT_CHECKBOX_DEFAULT_OPTIONS, MatCheckboxModule} from '@angular/material/checkbox';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatDividerModule} from '@angular/material/divider';
+import {MatExpansionModule} from '@angular/material/expansion';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
+import {MatListModule} from '@angular/material/list';
+import {MatSelectModule} from '@angular/material/select';
+import {MatSnackBarModule} from '@angular/material/snack-bar';
+import {MatToolbarModule} from '@angular/material/toolbar';
 import {ScrollingModule} from '@angular/cdk/scrolling';
 import {BundleSpecSelectItemComponent, CreateSaleComponent, OptionSelectItemComponent, OptionSelectModalComponent} from './sales';
 import {
@@ -27,7 +27,6 @@ import {
 import {CalendarModule, DateAdapter} from 'angular-calendar';
 import {adapterFactory} from 'angular-calendar/date-adapters/date-fns';
 import {AdminHourModalComponent} from './calendar/admin-hour-modal.component';
-import localeIt from '@angular/common/locales/it';
 import {SharedModule} from '../shared/shared.module';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import {MAT_DATE_LOCALE} from '@angular/material/core';
@@ -35,13 +34,12 @@ import {HAMMER_GESTURE_CONFIG, HammerGestureConfig, HammerModule} from '@angular
 import * as Hammer from 'hammerjs';
 import {HomeComponent} from './home';
 import {NgxMaterialTimepickerComponent, NgxMaterialTimepickerModule} from 'ngx-material-timepicker';
-import { StatsComponent } from './stats';
+import {StatsComponent} from './stats';
 import {ChartsModule} from 'ng2-charts';
 import {MatMenuModule} from '@angular/material/menu';
 import {MatTabsModule} from '@angular/material/tabs';
 
-registerLocaleData(localeIt);
-
+@Injectable()
 export class MyHammerConfig extends HammerGestureConfig {
     overrides = <any>{
         swipe: {direction: Hammer.DIRECTION_HORIZONTAL},
@@ -97,9 +95,10 @@ export class MyHammerConfig extends HammerGestureConfig {
         StatsComponent
     ],
     providers: [
-        { provide: MAT_CHECKBOX_CLICK_ACTION, useValue: 'check' },
+        { provide: MAT_CHECKBOX_DEFAULT_OPTIONS, useValue: 'check' },
         { provide: MAT_DATE_LOCALE, useValue: 'it-IT' },
         { provide: HAMMER_GESTURE_CONFIG, useClass: MyHammerConfig },
+        { provide: LOCALE_ID, useValue: 'it-IT' }
     ],
     entryComponents: [
         AdminHeaderModalComponent,
