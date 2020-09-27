867d0858d0d3b3b515faa7564dce4c44e869d2f6
daniel <dnlmrls9@gmail.com>
2020-09-27 17:05:23 +0200

    refresh button and some fix

--- web/src/app/services/global-error-handler.service.ts
+++ web/src/app/services/global-error-handler.service.ts
@@ -2,6 +2,7 @@ import {ErrorHandler, Injectable, Injector} from '@angular/core';
 import {Router} from '@angular/router';
 import {environment} from '../../environments/environment.prod';
 import {HttpClient} from "@angular/common/http";
+import {first} from "rxjs/operators";
 
 @Injectable()
 export class GlobalErrorHandler implements ErrorHandler {
@@ -21,15 +22,18 @@ export class GlobalErrorHandler implements ErrorHandler {
         let message = (err.message) ? err.message : (err.error) ? (err.error.message) ? err.error.message : err : err;
         message += '\n' + err.stack;
 
-        this.http.post('/log/error/', message);
-
-
-        this.router.navigate(['/error'], {
-            replaceUrl: true,
-            queryParams: {
-                message: message
-            }
+        this.http.post('/log/error/', message)
+            .pipe(first()).subscribe(_ => {
+            this.router.navigate(['/error'], {
+                replaceUrl: true,
+                queryParams: {
+                    message: message
+                }
+            });
         });
+
+
+
     }
 
 }
