0a4f835cfcfe32b61ee216c3d9f63a8b1d071d1f
daniel <dnlmrls9@gmail.com>
2021-01-13 12:49:04 +0100

    install terraform as well

--- .travis/install.sh
+++ .travis/install.sh
@@ -1,20 +1,21 @@
 #!/usr/bin/env bash
 
+echo "========================="
+echo "|  Installing MAVEN     |"
+echo "========================="
 wget "https://archive.apache.org/dist/maven/maven-3/${MVN_VERSION}/binaries/apache-maven-${MVN_VERSION}-bin.zip"
 unzip -qq "apache-maven-${MVN_VERSION}-bin.zip"
 
-if [[ "${TRAVIS_BRANCH}" = "master" ]]; then
-    echo "========================="
-    echo "|  Installing AWS-CLI   |"
-    echo "========================="
-    sudo snap install aws-cli --classic
-    echo "========================="
-    echo "|  Installing Terraform |"
-    echo "========================="
-    TF_ZIP="terraform_${TF_VERSION}_linux_amd64.zip"
-    TF_URI="https://releases.hashicorp.com/terraform/${TF_VERSION}/${TF_ZIP}"
-    wget "${TF_URI}"
-    unzip "${TF_ZIP}"
-fi
+echo "========================="
+echo "|  Installing AWS-CLI   |"
+echo "========================="
+sudo snap install aws-cli --classic
+
+echo "========================="
+echo "|  Installing Terraform |"
+echo "========================="
+TF_ZIP="terraform_${TF_VERSION}_linux_amd64.zip"
+wget "https://releases.hashicorp.com/terraform/${TF_VERSION}/${TF_ZIP}"
+unzip "${TF_ZIP}"
 
 
